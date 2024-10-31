3f04a7751064e7e7d4b2e14c7ead91def240f351
Chae <oumuamua121@gmail.com>
2024-11-01 00:17:59 +0900

    Feat : setting gradle build

--- friends_server/build.gradle.kts
+++ friends_server/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
 }
@@ -19,12 +21,47 @@ repositories {
 }
 
 dependencies {
+    // Spring
+   // implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+    // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+
+    //DB
+//    runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.21.0.0")
+//    implementation("org.redisson:redisson-spring-boot-starter:3.30.0")
+//    implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.4.1")
+//    implementation("com.linecorp.kotlin-jdsl:jpql-render:3.4.1")
+//    implementation("com.linecorp.kotlin-jdsl:spring-data-jpa-support:3.4.1")
+//    implementation("com.linecorp.kotlin-jdsl:hibernate-kotlin-jdsl-jakarta:2.2.1.RELEASE")
+//    implementation("org.hibernate.orm:hibernate-spatial:6.5.2.Final")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    // Test
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.mockk:mockk:1.13.11")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.0")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
+    testImplementation("org.testcontainers:testcontainers:1.19.8")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.8")
+    testImplementation("org.testcontainers:oracle-xe:1.19.8")
+    testImplementation("com.redis:testcontainers-redis:2.2.2")
+    testImplementation("org.testcontainers:localstack:1.19.8")
 }
 
 kotlin {
@@ -35,4 +72,10 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+// https://github.com/Ninja-Squad/springmockk/blob/master/README.md#gotchas
+tasks.test {
+    jvmArgs("--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED")
+    jvmArgs("--add-opens", "java.base/java.time=ALL-UNNAMED")
 }
