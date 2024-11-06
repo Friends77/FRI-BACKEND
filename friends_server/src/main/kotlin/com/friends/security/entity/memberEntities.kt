fb03bc50c6739c9c54067ff63a5b3fa08110a7c1
Hosung <an3735297@pusan.ac.kr>
2024-11-06 18:44:22 +0900

    Feat : @GeneratedValue의 전략을 Identity 로 변경

--- friends_server/src/main/kotlin/com/friends/security/entity/memberEntities.kt
+++ friends_server/src/main/kotlin/com/friends/security/entity/memberEntities.kt
@@ -4,7 +4,7 @@ import jakarta.persistence.*
 
 @Entity
 class Member(
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     val id: Long? = null,  // id는 불변 값으로 설정하여 JPA에서 자동으로 할당
     var name: String,
@@ -85,7 +85,7 @@ class Member(
 
 @Entity
 class Authority(
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "authority_id")
     val id: Long? = null,  // id는 불변 값으로 설정하여 JPA에서 자동으로 할당
     @Enumerated(EnumType.STRING)
