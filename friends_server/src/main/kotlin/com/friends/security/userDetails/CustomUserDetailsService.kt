fcab3522540863772afa391fef4e1032e1b8d355
HOSUNG <an3735297@pusan.ac.kr>
2024-11-07 23:23:03 +0900

    Feat : UserDetails, UserDetailsService 구현
    Spring Security 는 이메일, 패스워드 기반 인증 인가를 진행할 때 인증 주체로 Member Entity 가 아닌 UserDetails 인터페이스를 사용합니다. 따라서 멤버 엔티티를 기반으로 이를 구현해줘야합니다.

---
+++ friends_server/src/main/kotlin/com/friends/security/userDetails/CustomUserDetailsService.kt
@@ -0,0 +1,18 @@
+package com.friends.security.userDetails
+
+import com.friends.security.entity.Member
+import com.friends.security.repository.MemberRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class CustomUserDetailsService(
+        private val memberRepository: MemberRepository
+) : UserDetailsService{
+    override fun loadUserByUsername(email: String): UserDetails {
+        val member : Member = memberRepository.findByEmail(email) ?: throw UsernameNotFoundException("존재하지 않는 이메일입니다.")
+        return CustomUserDetails(member)
+    }
+}
