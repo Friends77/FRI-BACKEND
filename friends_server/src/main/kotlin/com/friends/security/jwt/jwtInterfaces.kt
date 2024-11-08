4c69b7011d9b564ff9bdf16eb45f04f5c3e601b8
HOSUNG <an3735297@pusan.ac.kr>
2024-11-08 14:04:01 +0900

    Feat : jwt 관련 추상 클래스로 구현
    사용 목적이 서비스 로직 보다는 JWT와 관련해서 유틸성으로 사용되는 것 같아 인터페이스가 아닌 추상 클래스로 구현하였습니다.
    
    이는 임의로 작성된 것이고 JWT 를 구현 방식에 따라 변경하겠습니다.

---
+++ friends_server/src/main/kotlin/com/friends/security/jwt/jwtInterfaces.kt
@@ -0,0 +1,42 @@
+package com.friends.security.jwt
+
+import org.springframework.security.core.GrantedAuthority
+
+abstract class JwtClaimReader {
+    companion object {
+        fun memberId(token: String) : Long {
+            TODO("Not yet implemented")
+        }
+        fun authorities(token: String) : Collection<GrantedAuthority> {
+            TODO("Not yet implemented")
+        }
+    }
+}
+
+abstract class JwtCreater {
+    companion object {
+        fun createAccessToken(memberId: Long, authorities: Collection<GrantedAuthority>) : String {
+            TODO("Not yet implemented")
+        }
+
+        fun createRefreshToken(memberId: Long, authorities: Collection<GrantedAuthority>) : String{
+            TODO("Not yet implemented")
+        }
+    }
+
+}
+
+abstract class JwtValidator {
+    companion object {
+        fun accessToken(token: String) : Boolean {
+            TODO("Not yet implemented")
+        }
+
+        fun refreshToken(token: String) : Boolean {
+            TODO("Not yet implemented")
+        }
+    }
+}
+
+class InvalidTokenException(s: String) : RuntimeException() {
+}
