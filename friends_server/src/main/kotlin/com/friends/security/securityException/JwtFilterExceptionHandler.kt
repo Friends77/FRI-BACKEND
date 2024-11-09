7d669de0f726addc62fc4fc0b13cf6a8461c0cd4
Chae <oumuamua121@gmail.com>
2024-11-10 01:30:45 +0900

    Feat : JwtFilterExceptionHandler 원위치로

--- friends_server/src/main/kotlin/com/friends/security/securityException/JwtFilterExceptionHandler.kt
+++ friends_server/src/main/kotlin/com/friends/security/securityException/JwtFilterExceptionHandler.kt
@@ -1,4 +1,4 @@
-package com.friends.common.exception
+package com.friends.security.securityException
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
