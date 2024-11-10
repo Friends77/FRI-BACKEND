6786440c9fa935f66aadc019cc0c0863c0e13316
djsumin <judy3504@naver.com>
2024-11-11 08:15:22 +0900

    Refactor: controller, service 단 분리

--- friends_server/src/main/kotlin/com/friends/oauth2/controller/OAuth2Controller.kt
+++ friends_server/src/main/kotlin/com/friends/oauth2/controller/OAuth2Controller.kt
@@ -1,82 +1,25 @@
 package com.friends.oauth2.controller
 
-import com.friends.config.OAuthConfig
+import com.friends.oauth2.service.OAuth2Service
 import com.friends.security.entity.OAuth2Provider
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 
 @RestController
 @RequestMapping("/api/auth")
-class OAuth2Controller (
-    private val webClient: WebClient.Builder
+class OAuth2Controller(
+    private val oAuth2Service: OAuth2Service
 ) {
-
-    @Value("\${google.client-id}")
-    private lateinit var googleClientId: String
-
-    @Value("\${google.client-secret}")
-    private lateinit var googleClientSecret: String
-
-    @Value("\${google.redirect-uri}")
-    private lateinit var googleRedirectUri: String
-
-    @Value("\${naver.client-id}")
-    private lateinit var naverClientId: String
-
-    @Value("\${naver.client-secret}")
-    private lateinit var naverClientSecret: String
-
-    @Value("\${naver.redirect-uri}")
-    private lateinit var naverRedirectUri: String
-
-
     /*
     * 0. 인가코드 발급 후 액세스 토큰 요청
     */
 
     @PostMapping("/oauth2-token")
     fun requestAccessToken(@RequestParam code: String, @RequestParam provider: OAuth2Provider): Mono<ResponseEntity<String>>{
-
         println("인가코드: $code, 공급자: $provider")
-
-        val config = when (provider) {
-            OAuth2Provider.GOOGLE -> OAuthConfig(
-                googleClientId,
-                googleClientSecret,
-                googleRedirectUri,
-                "https://oauth2.googleapis.com/token"
-            )
-
-            OAuth2Provider.NAVER -> OAuthConfig(
-                naverClientId,
-                naverClientSecret,
-                naverRedirectUri,
-                "https://nid.naver.com/oauth2.0/token"
-            )
-        }
-
-        return webClient.build()
-            .post()
-            .uri(config.tokenUri)
-            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-            .bodyValue(
-                "code=$code&client_id=${config.clientId}&client_secret=${config.clientSecret}&redirect_uri=${config.redirectUri}&grant_type=authorization_code"
-            )
-            .retrieve()
-            .bodyToMono(String::class.java)
-            .map { tokenResponse ->
-                println("액세스 토큰 요청 성공: $tokenResponse")
-                ResponseEntity.ok("Access Token Response: $tokenResponse")
-            }
-            .onErrorResume { e ->
-                println("액세스 토큰 요청 실패: ${e.message}")
-                Mono.just(ResponseEntity.badRequest().body("Failed to get token"))
-            }
+        return oAuth2Service.requestAccessToken(code, provider)
     }
 
     /*
@@ -84,28 +27,7 @@ class OAuth2Controller (
     */
     @GetMapping("/oauth2-data")
     fun getUserData(@RequestParam accessToken: String, @RequestParam provider: OAuth2Provider): Mono<ResponseEntity<String>> {
-
-        val userInfoUri = when (provider) {
-            OAuth2Provider.GOOGLE -> "https://www.googleapis.com/oauth2/v2/userinfo"
-            OAuth2Provider.NAVER -> "https://openapi.naver.com/v1/nid/me"
-        }
-
-        return webClient.build()
-            .get()
-            .uri(userInfoUri) { uriBuilder ->
-                uriBuilder.queryParam("access_token", accessToken).build()
-            }
-            .retrieve()
-            .bodyToMono(String::class.java)
-            .map { userDataResponse ->
-                println("사용자 데이터 요청 성공: $userDataResponse")
-                ResponseEntity.ok("User data Response: $userDataResponse")
-            }
-            .onErrorResume { e ->
-                println("사용자 데이터 요청 실패: ${e.message}")
-                Mono.just(ResponseEntity.status(500).body("Failed to get user data"))
-            }
-
+        return oAuth2Service.getUserData(accessToken, provider)
     }
 }
 
--- friends_server/src/main/kotlin/com/friends/oauth2/service/OAuth2Service.kt
+++ friends_server/src/main/kotlin/com/friends/oauth2/service/OAuth2Service.kt
@@ -1,9 +1,99 @@
 package com.friends.oauth2.service
 
+import com.friends.config.OAuthConfig
+import com.friends.security.entity.OAuth2Provider
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
 @Service
-class OAuth2Service {
+class OAuth2Service (
+    private val webClient: WebClient.Builder
+){
+
+    @Value("\${google.client-id}")
+    private lateinit var googleClientId: String
+
+    @Value("\${google.client-secret}")
+    private lateinit var googleClientSecret: String
+
+    @Value("\${google.redirect-uri}")
+    private lateinit var googleRedirectUri: String
+
+    @Value("\${naver.client-id}")
+    private lateinit var naverClientId: String
+
+    @Value("\${naver.client-secret}")
+    private lateinit var naverClientSecret: String
+
+    @Value("\${naver.redirect-uri}")
+    private lateinit var naverRedirectUri: String
+
+    /*
+    * access Token 발급 요청
+    * */
+    fun requestAccessToken(code: String, provider: OAuth2Provider): Mono<ResponseEntity<String>>{
+        val config = when(provider){
+            OAuth2Provider.GOOGLE -> OAuthConfig(
+                googleClientId,
+                googleClientSecret,
+                googleRedirectUri,
+                "https://oauth2.googleapis.com/token"
+            )
+            OAuth2Provider.NAVER -> OAuthConfig(
+                naverClientId,
+                naverClientSecret,
+                naverRedirectUri,
+                "https://nid.naver.com/oauth2.0/token"
+            )
+        }
+        return webClient.build()
+            .post()
+            .uri(config.tokenUri)
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .bodyValue(
+                "code=$code&client_id=${config.clientId}&client_secret=${config.clientSecret}&redirect_uri=${config.redirectUri}&grant_type=authorization_code"
+            )
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .map { tokenResponse ->
+                println("액세스 토큰 요청 성공: $tokenResponse")
+                ResponseEntity.ok("Access Token Response: $tokenResponse")
+            }
+            .onErrorResume { e ->
+                println("액세스 토큰 요청 실패: ${e.message}")
+                Mono.just(ResponseEntity.badRequest().body("Failed to get token"))
+            }
+    }
+
+    /*
+    유저 데이터 요청
+    */
+    fun getUserData(accessToken: String, provider: OAuth2Provider): Mono<ResponseEntity<String>>{
+        val userInfoUri = when (provider) {
+            OAuth2Provider.GOOGLE -> "https://www.googleapis.com/oauth2/v2/userinfo"
+            OAuth2Provider.NAVER -> "https://openapi.naver.com/v1/nid/me"
+        }
+
+        return webClient.build()
+            .get()
+            .uri(userInfoUri) { uriBuilder ->
+                uriBuilder.queryParam("access_token", accessToken).build()
+            }
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .map { userDataResponse ->
+                println("사용자 데이터 요청 성공: $userDataResponse")
+                ResponseEntity.ok("User data Response: $userDataResponse")
+            }
+            .onErrorResume { e ->
+                println("사용자 데이터 요청 실패: ${e.message}")
+                Mono.just(ResponseEntity.status(500).body("Failed to get user data"))
+            }
+    }
 
 
 
