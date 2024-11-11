62f8f12061ecaa92b29ae9e4c8adccf621dc53be
Hosung <an3735297@pusan.ac.kr>
2024-11-11 18:05:49 +0900

    feat : jwtFilter

---
+++ friends_server/src/main/kotlin/com/friends/security/filter/JwtFilter.kt
@@ -0,0 +1,36 @@
+package com.friends.security.filter
+
+import com.friends.security.authentication.AuthenticationCreator
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class JwtFilter(
+    private val authenticationCreator: AuthenticationCreator,
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        resolveToken(request)?.let {
+            val authentication: Authentication = authenticationCreator.createByAccessToken(it)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        filterChain.doFilter(request, response)
+    }
+
+    private fun resolveToken(request: HttpServletRequest): String? {
+        val bearerToken = request.getHeader("Authorization")
+        return if (!bearerToken.isNullOrEmpty() && bearerToken.startsWith("Bearer ")) {
+            bearerToken.substring(7)
+        } else {
+            null
+        }
+    }
+}
