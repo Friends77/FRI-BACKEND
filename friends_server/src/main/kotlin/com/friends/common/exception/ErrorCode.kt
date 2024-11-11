461dd6db39c99022914da2472c60ecb03a7aaf77
Chae <oumuamua121@gmail.com>
2024-11-11 14:26:24 +0900

    Feat : Errorcode 변경

--- friends_server/src/main/kotlin/com/friends/common/exception/ErrorCode.kt
+++ friends_server/src/main/kotlin/com/friends/common/exception/ErrorCode.kt
@@ -9,8 +9,8 @@ enum class ErrorCode(val httpStatus: HttpStatus, val code: Int, val errorMessage
 
     // Auth API error 11000대
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, -11001, "유효하지 않은 토큰입니다."),
-    MISSING_SOCIAL_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, -11001, "SocialAccessToken 이 존재하지 않습니다."),
     MISSING_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, -11002, "RefreshToken 이 존재하지 않습니다."),
+    MISSING_SOCIAL_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, -11003, "SocialAccessToken 이 존재하지 않습니다."),
 
     // Member API error 12000대
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, -12001, "존재하지 않는 회원입니다."),
