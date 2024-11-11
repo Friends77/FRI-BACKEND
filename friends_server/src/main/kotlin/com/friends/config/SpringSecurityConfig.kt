92d3e74bae1de810acc1bf63c608a93768b49a48
Hosung <an3735297@pusan.ac.kr>
2024-11-11 18:07:37 +0900

    feat : spring security 설정

---
+++ friends_server/src/main/kotlin/com/friends/config/SpringSecurityConfig.kt
@@ -0,0 +1,65 @@
+package com.friends.config
+
+import com.friends.member.entity.Role
+import com.friends.security.filter.JwtFilter
+import com.friends.security.securityException.JwtFilterAccessDeniedHandler
+import com.friends.security.securityException.JwtFilterAuthenticationEntryPoint
+import com.friends.security.userDetails.CustomUserDetailsService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SpringSecurityConfig(
+    private val customUserDetailsService: CustomUserDetailsService,
+    private val jwtFilterAccessDeniedHandler: JwtFilterAccessDeniedHandler,
+    private val jwtFilterAuthenticationEntryPoint: JwtFilterAuthenticationEntryPoint,
+    private val jwtFilter: JwtFilter,
+) {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf { it.disable() }
+
+        http
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling {
+                it
+                    .accessDeniedHandler(jwtFilterAccessDeniedHandler)
+                    .authenticationEntryPoint(jwtFilterAuthenticationEntryPoint)
+            }
+
+        http.authorizeHttpRequests {
+            it
+                .requestMatchers("/api/user/**")
+                .hasAuthority(Role.ROLE_USER.name)
+                .requestMatchers("/api/admin/**")
+                .hasAuthority(Role.ROLE_ADMIN.name)
+                .anyRequest()
+                .permitAll()
+        }
+
+        return http.build()
+    }
+
+    @Bean
+    fun authenticationProvider(): DaoAuthenticationProvider =
+        DaoAuthenticationProvider().apply {
+            setUserDetailsService(customUserDetailsService)
+            setPasswordEncoder(passwordEncoder())
+        }
+
+    @Bean
+    fun authenticationManager(authConfig: AuthenticationConfiguration): AuthenticationManager = authConfig.authenticationManager
+}
