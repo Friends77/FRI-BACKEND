42683a5c75ec87f39085687e08f603350ebc5b42
HOSUNG <an3735297@pusan.ac.kr>
2024-11-13 19:02:09 +0900

    Feat : jwt Service
    jwt 생성, 읽기, 검증

--- friends_server/src/main/kotlin/com/friends/jwt/JwtServices.kt
+++ friends_server/src/main/kotlin/com/friends/jwt/JwtServices.kt
@@ -1,6 +1,12 @@
 package com.friends.jwt
 
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.stereotype.Component
+import java.util.Date
 
 /**
  * JWT 설정 정보를 담고 있는 클래스입니다.
@@ -12,3 +18,80 @@ class JwtProperties(
     val accessTokenExpiration: Long,
     val refreshTokenExpiration: Long,
 )
+
+@Component
+class JwtService(
+    val jwtProperties: JwtProperties,
+) {
+    val key by lazy { Keys.hmacShaKeyFor(jwtProperties.secretKey.toByteArray()) }
+
+    private fun getDateAfterSeconds(seconds: Long) = Date(System.currentTimeMillis() + seconds * 1000)
+
+    fun createAccessToken(
+        memberId: Long,
+        authorities: Collection<GrantedAuthority>,
+    ): String =
+        Jwts
+            .builder()
+            .claim("memberId", memberId.toString())
+            .claim("authorities", authorities.map { it.authority })
+            .signWith(key)
+            .expiration(getDateAfterSeconds(jwtProperties.accessTokenExpiration))
+            .compact()
+
+    fun createRefreshToken(
+        memberId: Long,
+        authorities: Collection<GrantedAuthority>,
+    ): String =
+        Jwts
+            .builder()
+            .claim("memberId", memberId.toString())
+            .claim("authorities", authorities.map { it.authority })
+            .signWith(key)
+            .expiration(getDateAfterSeconds(jwtProperties.refreshTokenExpiration))
+            .compact()
+
+    fun getMemberId(token: String): Long =
+        Jwts
+            .parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .payload
+            .get("memberId", String::class.java)
+            .toLong()
+
+    fun getAuthorities(token: String): Collection<GrantedAuthority> {
+        val result = mutableListOf<SimpleGrantedAuthority>()
+        val authorities =
+            Jwts
+                .parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .payload
+                .get("authorities", List::class.java)
+        authorities.forEach { result.add(SimpleGrantedAuthority(it.toString())) }
+        return result
+    }
+
+    fun getExpiration(token: String): Date =
+        Jwts
+            .parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .payload.expiration
+
+    fun validate(token: String): Boolean =
+        try {
+            Jwts
+                .parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+            true
+        } catch (e: Exception) {
+            false
+        }
+}
