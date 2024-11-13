7f73c7e134fd4bf6d076d60a0ed8ce35c0b6d74b
HOSUNG <an3735297@pusan.ac.kr>
2024-11-13 19:03:16 +0900

    Feat : access token, refresh token 을 redis 에 저장

---
+++ friends_server/src/main/kotlin/com/friends/jwt/tokenRepositories.kt
@@ -0,0 +1,42 @@
+package com.friends.jwt
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Repository
+import java.util.concurrent.TimeUnit
+
+@Repository
+class AuthJwtRepository(
+    private val redisTemplate: RedisTemplate<String, String>,
+    private val jwtProperties: JwtProperties,
+) {
+    private fun getAccessTokenKey(accessToken: String) = "accessToken:$accessToken"
+
+    private fun getRefreshTokenKey(refreshToken: String) = "refreshToken:$refreshToken"
+
+    /**
+     * accessToken과 refreshToken을 저장합니다.
+     * accessToken은 accessTokenExpiration 시간 동안, refreshToken은 refreshTokenExpiration 시간 동안 저장되고
+     * 시간이 지나면 자동으로 삭제됩니다.
+     */
+    fun save(
+        accessToken: String,
+        refreshToken: String,
+    ) {
+        redisTemplate.opsForValue().set(
+            getAccessTokenKey(accessToken),
+            refreshToken,
+            jwtProperties.accessTokenExpiration,
+            TimeUnit.SECONDS,
+        )
+        redisTemplate.opsForValue().set(
+            getRefreshTokenKey(refreshToken),
+            accessToken,
+            jwtProperties.refreshTokenExpiration,
+            TimeUnit.SECONDS,
+        )
+    }
+
+    fun getAccessToken(refreshToken: String): String? = redisTemplate.opsForValue().get(getRefreshTokenKey(refreshToken))
+
+    fun getRefreshToken(accessToken: String): String? = redisTemplate.opsForValue().get(getAccessTokenKey(accessToken))
+}
