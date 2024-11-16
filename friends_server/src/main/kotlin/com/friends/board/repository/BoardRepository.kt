3b06902d9ccffbd89627d3b29c8632731781f878
djsumin <judy3504@naver.com>
2024-11-16 15:46:29 +0900

    Feat: board_hashtag entity, hashtag entity 구현

--- friends_server/src/main/kotlin/com/friends/board/repository/BoardRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.friends.board.repository
-
-import com.friends.board.entity.BoardEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface BoardRepository: JpaRepository<BoardEntity, Long> {
-}
