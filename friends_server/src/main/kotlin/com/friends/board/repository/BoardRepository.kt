0931f3b60153eb871207afd403dad06c62301c3a
djsumin <judy3504@naver.com>
2024-11-16 15:49:50 +0900

    Feat: BoardService 구현

---
+++ friends_server/src/main/kotlin/com/friends/board/repository/BoardRepository.kt
@@ -0,0 +1,9 @@
+package com.friends.board.repository
+
+import com.friends.board.entity.Board
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BoardRepository: JpaRepository<Board, Long> {
+}
