f14c780f264c873f0222944fdacdf806353e6a0e
djsumin <judy3504@naver.com>
2024-11-16 15:49:50 +0900

    Feat: BoardService 구현

---
+++ friends_server/src/main/kotlin/com/friends/board/service/BoardService.kt
@@ -0,0 +1,44 @@
+package com.friends.board.service
+
+import com.friends.board.dto.BoardFormDto
+import com.friends.board.entity.Board
+import com.friends.board.repository.BoardRepository
+import com.friends.member.repository.MemberRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class BoardService @Autowired constructor(
+    val boardRepository: BoardRepository,
+    val memberRepository: MemberRepository
+){
+    fun save(boardFormDto: BoardFormDto): Long?{
+        val member = memberRepository.findById(boardFormDto.memberId)
+            .orElseThrow { IllegalArgumentException("Member not found with id: ${boardFormDto.memberId}")}
+
+        val board = Board(
+            member = member,
+            content = boardFormDto.content,
+        )
+
+        return boardRepository.save(board).id
+    }
+
+    fun getBoard(id: Long): Board {
+        return boardRepository.findById(id).get()
+    }
+
+    fun deleteBoard(id: Long){
+        return boardRepository.deleteById(id)
+    }
+
+    fun updateBoard(id: Long, boardFormDto: BoardFormDto): Board {
+        val board = boardRepository.findById(id).get()
+        board.updateBoard(boardFormDto)
+        return board
+    }
+
+    fun getBoardList(): List<Board>{
+        return boardRepository.findAll()
+    }
+}
