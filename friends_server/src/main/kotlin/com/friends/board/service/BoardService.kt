04876eac3e8c06fea2c8af21d6f093e02efc1d53
djsumin <judy3504@naver.com>
2024-11-18 21:06:32 +0900

    Refactor: boardService 리팩토링

--- friends_server/src/main/kotlin/com/friends/board/service/BoardService.kt
+++ friends_server/src/main/kotlin/com/friends/board/service/BoardService.kt
@@ -4,8 +4,11 @@ import com.friends.board.dto.BoardFormDto
 import com.friends.board.entity.Board
 import com.friends.board.repository.BoardRepository
 import com.friends.member.repository.MemberRepository
-import org.springframework.beans.factory.annotation.Autowired
+import jakarta.transaction.Transactional
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class BoardService
@@ -13,10 +16,13 @@ class BoardService
         val boardRepository: BoardRepository,
         val memberRepository: MemberRepository,
     ) {
+        @Transactional
         fun save(boardFormDto: BoardFormDto): Board  {
             val member =
                 memberRepository.findById(boardFormDto.memberId)
-                    .orElseThrow { IllegalArgumentException("Member not found with id: ${boardFormDto.memberId}") }
+                    .orElseThrow {
+                        ResponseStatusException(HttpStatus.NOT_FOUND, "Member not found with id: ${boardFormDto.memberId}")
+                    }
 
             val board =
                 Board(
@@ -28,11 +34,18 @@ class BoardService
         }
 
         fun getBoard(id: Long): Board {
+
+            val board = boardRepository.findById(id)
+                .orElseThrow{
+                    ResponseStatusException(HttpStatus.NOT_FOUND, "Board not found with id: $id")
+                }
+
             return boardRepository.findById(id).get()
         }
 
+        @Transactional
         fun deleteBoard(id: Long)  {
-            return boardRepository.deleteById(id)
+            boardRepository.deleteById(id)
         }
 
         fun updateBoard(
