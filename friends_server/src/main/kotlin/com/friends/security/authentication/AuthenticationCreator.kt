5504991eecc455224e49a85f4bd08de1d0903ba8
HOSUNG <an3735297@pusan.ac.kr>
2024-11-18 19:06:04 +0900

    fix : refactoring 으로 인한 잘못된 의존관계 수정

--- friends_server/src/main/kotlin/com/friends/security/authentication/AuthenticationCreator.kt
+++ friends_server/src/main/kotlin/com/friends/security/authentication/AuthenticationCreator.kt
@@ -1,17 +1,17 @@
 package com.friends.security.authentication
 
-import com.friends.jwt.JwtService
+import com.friends.jwt.AtRtService
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
 
 @Component
 class AuthenticationCreator(
-    private val jwtService: JwtService,
+    private val atRtService: AtRtService,
 ) {
     fun createByAccessToken(accessToken: String): Authentication {
-        val memberId = jwtService.getMemberId(accessToken)
-        val authorities = jwtService.getAuthorities(accessToken)
+        val memberId = atRtService.getMemberId(accessToken)
+        val authorities = atRtService.getAuthorities(accessToken)
         return UsernamePasswordAuthenticationToken(memberId, accessToken, authorities)
     }
 }
