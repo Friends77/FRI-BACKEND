dfdf335ebb1dec0a01e857bd1c3649f4a9504d19
HOSUNG <an3735297@pusan.ac.kr>
2024-11-22 13:48:55 +0900

    feat : gradle 의존성 추가 [spring-email, thymeleaf]

--- friends_server/build.gradle.kts
+++ friends_server/build.gradle.kts
@@ -50,6 +50,11 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
     implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
+    // email
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    // thyemleaf
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
     // configuration property
     implementation("org.springframework.boot:spring-boot-configuration-processor")
 
