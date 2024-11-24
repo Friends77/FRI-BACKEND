faa8517d5763dbbb36ee111f4c672cdaa930e117
djsumin <judy3504@gmail.com>
2024-11-24 16:59:51 +0900

    feat: boardQueryService test 중 조회 테스트

---
+++ friends_server/src/test/kotlin/com/friends/board/service/BoardCommandServiceTest.kt
@@ -0,0 +1,5 @@
+package com.friends.board.service
+
+import org.junit.jupiter.api.Assertions.*
+
+class BoardCommandServiceTest
---
+++ friends_server/src/test/kotlin/com/friends/board/service/BoardQueryServiceTest.kt
@@ -0,0 +1,47 @@
+package com.friends.board.service
+
+import com.friends.board.entity.Board
+import com.friends.board.entity.BoardHashtag
+import com.friends.board.entity.Hashtag
+import com.friends.board.repository.BoardHashtagRepository
+import com.friends.board.repository.BoardRepository
+import com.friends.member.entity.Member
+import com.friends.member.entity.OAuth2Provider
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.springframework.data.repository.findByIdOrNull
+
+class BoardQueryServiceTest :
+        BehaviorSpec({
+            val boardQueryService = mockk<BoardQueryService>()
+            val boardRepository = mockk<BoardRepository>()
+            val boardHashtagRepository = mockk<BoardHashtagRepository>()
+
+            isolationMode = IsolationMode.InstancePerLeaf
+
+            given("유효한 boardId가 주어졌을 때"){
+                val boardId = 1L
+                val member = Member(id = 1L, name = "Test Member", email = "test@test.com", password = "1234", oauth2Provider = OAuth2Provider.GOOGLE, imageUrl = "test imageurl")
+                val mockBoard = Board(id = boardId, member = member, content = "Test content")
+                val mockHashtags = listOf(
+                    BoardHashtag(id = 1L, board = mockBoard, hashtag = Hashtag(id = 1L, tag = "abc"))
+                )
+
+                every { boardRepository.findByIdOrNull(boardId) } returns mockBoard
+                every { boardHashtagRepository.findByBoard(mockBoard) } returns mockHashtags
+
+                `when`("getBoard 메서드를 호출하면"){
+                    val result = boardQueryService.getBoard(boardId)
+
+                    then("Board와 Board 관련 Hashtag 리스트를 반환해야 한다.") {
+                        result?.first shouldBe mockBoard
+                        result?.second shouldContainExactly mockHashtags
+                    }
+                }
+            }
+
+        })
