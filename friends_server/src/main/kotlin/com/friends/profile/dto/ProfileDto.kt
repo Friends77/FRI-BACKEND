3103d229b71604322b256f1c2351dcc8c070d497
djsumin <judy3504@gmail.com>
2024-11-26 08:25:58 +0900

    feat: profileService 상세조회

---
+++ friends_server/src/main/kotlin/com/friends/profile/dto/ProfileDto.kt
@@ -0,0 +1,16 @@
+package com.friends.profile.dto
+
+import com.friends.profile.entity.GenderEnum
+import com.friends.profile.entity.MbtiEnum
+import java.util.Date
+
+data class ProfileDto (
+    val nickname: String,
+    var birth: Date? = null,
+    var gender: GenderEnum? = null,
+    var location: String? = null,
+    var selfDescription: String? = null,
+    var mbti: MbtiEnum? = null,
+    var interestTag: List<String>? = null,
+    var hobbyTag: List<String>? = null,
+)
---
+++ friends_server/src/main/kotlin/com/friends/profile/service/ProfileQueryService.kt
@@ -0,0 +1,29 @@
+package com.friends.profile.service
+
+import com.friends.member.repository.MemberRepository
+import com.friends.profile.dto.ProfileDto
+import com.friends.profile.repository.ProfileRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class ProfileQueryService (
+    val profileRepository: ProfileRepository,
+    val memberRepository: MemberRepository,
+) {
+
+    //프로필 상세조회
+    @Transactional(readOnly = true)
+    fun getProfile(requestMemberId: Long) : ProfileDto {
+        val member = memberRepository.findById(requestMemberId)
+            .orElseThrow { IllegalArgumentException("Member not found with id: $requestMemberId") }
+
+        val nickname = member.name
+        //회원가입 시 어떤 필드들이 채워지는지?
+        val profileRes = ProfileDto(
+            nickname = nickname,
+        )
+        return profileRes
+    }
+
+}
