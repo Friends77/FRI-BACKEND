f50b243ba93cddc8fcc01a08f307bb405a6261ca
djsumin <judy3504@gmail.com>
2024-11-25 22:07:44 +0900

    test: deleteBoard test

--- friends_server/src/test/kotlin/com/friends/board/service/BoardCommandServiceTest.kt
+++ friends_server/src/test/kotlin/com/friends/board/service/BoardCommandServiceTest.kt
@@ -2,6 +2,7 @@ package com.friends.board.service
 
 import com.friends.board.dto.BoardFormDto
 import com.friends.board.entity.Board
+import com.friends.board.entity.BoardHashtag
 import com.friends.board.entity.Hashtag
 import com.friends.board.repository.BoardHashtagRepository
 import com.friends.board.repository.BoardRepository
@@ -12,7 +13,6 @@ import com.friends.member.repository.MemberRepository
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 import io.mockk.every
 import io.mockk.mockk
 import java.util.*
@@ -43,8 +43,10 @@ class BoardCommandServiceTest :
             //1. createBoard
             given("유효한 데이터로 게시글을 작성하는 경우") {
                 every { memberRepository.findById(requestMemberId) } returns Optional.of(member)
+                every { boardRepository.save(any()) } answers { firstArg() }
                 every { hashtagRepository.findByTag(any()) } answers { null } //모든 해시태그는 새로 생성됩니다.
                 every { hashtagRepository.save(any()) } answers { firstArg() } //저장된 해시태그 반환
+                every { boardHashtagRepository.saveAll(any<List<BoardHashtag>>()) } answers { firstArg() }
 
                 `when` ("createBoard 메서드를 호출하면") {
                     val result = boardCommandService.createBoard(boardFormDto, requestMemberId)
@@ -60,7 +62,18 @@ class BoardCommandServiceTest :
 
 
             //2. deleteBoard
+            given("작성자가 쓴 게시글이 존재할 때") {
+                every { boardRepository.findById(mockBoard.id!!) } returns Optional.of(mockBoard)
+                every { boardRepository.deleteById(mockBoard.id!!) } returns Unit
 
+                `when`("deleteBoard 메서드를 호출하면") {
+                    val result = boardCommandService.deleteBoard(mockBoard.id!!, requestMemberId)
+
+                    then("게시글이 삭제되어야 한다.") {
+
+                    }
+                }
+            }
 
 
             //3. updateBoard
