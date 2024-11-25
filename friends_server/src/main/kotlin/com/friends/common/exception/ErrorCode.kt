16d91453dbd19d3e171406cd2785892911ddc4df
HOSUNG <an3735297@pusan.ac.kr>
2024-11-25 16:22:57 +0900

    feat : oauth2 서비스 에러 처리

--- friends_server/src/main/kotlin/com/friends/common/exception/ErrorCode.kt
+++ friends_server/src/main/kotlin/com/friends/common/exception/ErrorCode.kt
@@ -23,4 +23,9 @@ enum class ErrorCode(
     INVALID_EMAIL(HttpStatus.BAD_REQUEST, -13001, "유효하지 않은 이메일입니다."),
     INVALID_EMAIL_VERIFY_CODE(HttpStatus.UNAUTHORIZED, -13002, "유효하지 않은 이메일 인증 코드입니다."),
     SMTP_CONNECTION_FAILED(HttpStatus.SERVICE_UNAVAILABLE, -13003, "이메일 서버와의 연결에 실패했습니다."),
+
+    // OAuth2 API error 14000대
+    OAUTH2_ACCESS_TOKEN_FETCH_FAILED(HttpStatus.UNAUTHORIZED, -14001, "OAuth2 Access Token 획득에 실패했습니다."),
+    OAUTH2_USER_INFO_FETCH_FAILED(HttpStatus.UNAUTHORIZED, -14002, "OAuth2 User 정보 획득에 실패했습니다."),
+    OAUTH2_NULL_RESPONSE(HttpStatus.BAD_GATEWAY, -14003, "OAuth2 API로부터 응답이 없습니다."),
 }
