b9aad608b1945bdbd73584971ad01f4fe54e5564
Chae <oumuamua121@gmail.com>
2024-12-03 17:36:55 +0900

    Feat : RDB로 전환

--- friends_server/src/test/kotlin/com/friends/DBConnectionTest.kt
+++ friends_server/src/test/kotlin/com/friends/DBConnectionTest.kt
@@ -22,26 +22,16 @@ class DBConnectionTest(
                         }
                     }
                 }
-        }
-
-        given("Redis 연결 정보가 주어지면") {
-            `when`("Redis 연결 테스트를 수행하면") {
-                then("Bucket에 데이터가 정상적으로 저장되어야 한다") {
-                    shouldNotThrowAny {
-                        redissonClient.getBucket<String>("test").set("connection test")
-                    }
-                }
             }
-        }
 
-        given("MongoDB 연결 정보가 주어지면") {
-            `when`("MongoDB 연결 테스트를 수행하면") {
-                then("Database 목록을 조회할 수 있어야 한다") {
-                    shouldNotThrowAny {
-                        mongoClients.listDatabaseNames()
+            given("Redis 연결 정보가 주어지면") {
+                `when`("Redis 연결 테스트를 수행하면") {
+                    then("Bucket에 데이터가 정상적으로 저장되어야 한다") {
+                        shouldNotThrowAny {
+                            redissonClient.getBucket<String>("test").set("connection test")
                         }
                     }
                 }
             }
-    },
-)
+        },
+    )
