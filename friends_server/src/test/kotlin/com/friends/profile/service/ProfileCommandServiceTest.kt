621ac8792ad281689a625b07aae076d9fdd9246b
djsumin <judy3504@gmail.com>
2024-12-04 20:33:29 +0900

    chore: behavior spec

--- friends_server/src/test/kotlin/com/friends/profile/service/ProfileCommandServiceTest.kt
+++ friends_server/src/test/kotlin/com/friends/profile/service/ProfileCommandServiceTest.kt
@@ -1,5 +1,13 @@
 package com.friends.profile.service
 
-import org.junit.jupiter.api.Assertions.*
+import com.friends.member.repository.MemberRepository
+import com.friends.profile.repository.ProfileRepository
+import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.mockk
 
-class ProfileCommandServiceTest
+class ProfileCommandServiceTest :
+        BehaviorSpec({
+            val profileRepository = mockk<ProfileRepository>()
+            val memberRepository = mockk<MemberRepository>()
+            val profileCommandService = ProfileCommandService(profileRepository, memberRepository)
+        })
