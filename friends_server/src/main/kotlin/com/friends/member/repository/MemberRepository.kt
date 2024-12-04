72add127c4d85d981d8fddd7fc7d5be3c041e6fc
Chae <oumuamua121@gmail.com>
2024-12-04 21:24:02 +0900

    Feat : merge 후 에러나는 코드 수정

--- friends_server/src/main/kotlin/com/friends/member/repository/MemberRepository.kt
+++ friends_server/src/main/kotlin/com/friends/member/repository/MemberRepository.kt
@@ -1,11 +1,11 @@
 package com.friends.member.repository
 
-import com.friends.member.NotFoundMemberException
+import com.friends.member.MemberNotFoundException
 import com.friends.member.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-fun MemberRepository.getByMemberId(memberId: Long): Member = findById(memberId).orElseThrow { throw NotFoundMemberException(memberId) }
+fun MemberRepository.getByMemberId(memberId: Long): Member = findById(memberId).orElseThrow { throw MemberNotFoundException() }
 
 @Repository
 interface MemberRepository : JpaRepository<Member, Long> {
