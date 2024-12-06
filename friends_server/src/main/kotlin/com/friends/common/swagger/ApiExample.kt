0e0a9b5174d7455d3128a2b4d3f77680f8178df7
Chae <oumuamua121@gmail.com>
2024-12-06 16:08:00 +0900

    Feat : 주석 삭제

--- friends_server/src/main/kotlin/com/friends/common/swagger/ApiExample.kt
+++ friends_server/src/main/kotlin/com/friends/common/swagger/ApiExample.kt
@@ -2,20 +2,6 @@ package com.friends.common.swagger
 
 import io.swagger.v3.oas.models.examples.Example
 
-/*examples = Example().apply {
-        summary = "로그인 성공"
-        value = """
-            {
-                "status": "200",
-                "data": {
-                    "token": "123213"
-                }
-            }
-        """
-    },
-    code = 200,
-    name = "로그인 성공"*/
-// TODO : 지우기
 class ApiExample(
     val examples: Example, // 객체로 실제 예제 응답
     val code: Int, // 응답 코드
