475714c28511ddd5a8cf87b46b12192ea6bec111
djsumin <judy3504@gmail.com>
2024-12-07 15:57:54 +0900

    fix: test 통과 코드

--- friends_server/src/test/kotlin/com/friends/profile/service/ProfileCommandServiceTest.kt
+++ friends_server/src/test/kotlin/com/friends/profile/service/ProfileCommandServiceTest.kt
@@ -5,12 +5,10 @@ import com.friends.profile.MEMBER_ID
 import com.friends.profile.createTestMember
 import com.friends.profile.createTestProfile
 import com.friends.profile.createTestProfileCreateDto
-import com.friends.profile.entity.Profile
 import com.friends.profile.repository.ProfileRepository
 import com.friends.profile.updateTestProfile
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.ints.exactly
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
@@ -27,16 +25,21 @@ class ProfileCommandServiceTest :
 
             given("createProfile 메서드를 호출할 때") {
                 every { memberRepository.findById(MEMBER_ID) } returns Optional.of(createTestMember())
-                every { profileRepository.save(any()) } answers {firstArg<Profile>() }
-
-                val profile = createTestProfile()
+                every { profileRepository.save(any()) } returns createTestProfile()
 
                 `when`("유효한 프로필 정보를 전달하면") {
-                    val savedProfile = profileCommandService.createProfile(MEMBER_ID, createTestProfileCreateDto())
+                    profileCommandService.createProfile(MEMBER_ID, createTestProfileCreateDto())
 
                     then("프로필이 저장되어야 한다.") {
-                        savedProfile shouldBe profile
-                        verify(exactly = 1) { profileRepository.save(profile)}
+                        verify(exactly = 1) { memberRepository.findById(MEMBER_ID) }
+                        verify(exactly = 1) { profileRepository.save(match {
+                                    it.birth == createTestProfileCreateDto().birth &&
+                                    it.gender == createTestProfileCreateDto().gender &&
+                                    it.location == createTestProfileCreateDto().location &&
+                                    it.selfDescription == createTestProfileCreateDto().selfDescription &&
+                                    it.mbti == createTestProfileCreateDto().mbti &&
+                                    it.imageUrl == createTestProfileCreateDto().imageUrl
+                        })}
                     }
                 }
             }
@@ -45,15 +48,19 @@ class ProfileCommandServiceTest :
                 val existingProfile = createTestProfile()
                 val updatedProfile = updateTestProfile()
 
-                every { profileRepository.findById(existingProfile.id) } returns Optional.of(existingProfile)
-                every { profileRepository.findByMemberId(MEMBER_ID) } returns createTestProfile()
-                every { profileRepository.save(any()) } answers {firstArg<Profile>() }
+                every { profileRepository.findByMemberId(MEMBER_ID) } returns existingProfile
 
                 `when`("존재하는 프로필을 수정하면") {
-                    val savedProfile = profileCommandService.updateProfile(existingProfile.id, updatedProfile)
+                    profileCommandService.updateProfile(MEMBER_ID, updatedProfile)
 
                     then("수정된 프로필이 저장되어야 한다.") {
-                        savedProfile shouldBe updatedProfile
+                        verify(exactly = 1) { profileRepository.findByMemberId(MEMBER_ID) }
+                        existingProfile.birth shouldBe updatedProfile.birth
+                        existingProfile.gender shouldBe updatedProfile.gender
+                        existingProfile.location shouldBe updatedProfile.location
+                        existingProfile.selfDescription shouldBe updatedProfile.selfDescription
+                        existingProfile.mbti shouldBe updatedProfile.mbti
+                        existingProfile.imageUrl shouldBe updatedProfile.imageUrl
                     }
                 }
             }
