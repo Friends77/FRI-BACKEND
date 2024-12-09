22a434f8d6d84812213572a45435dfbfc0988637
HOSUNG <an3735297@pusan.ac.kr>
2024-12-09 17:46:45 +0900

    feat : postgis 플러그인 사용하여 n 미터 이내 데이터 조회 sql 작성

--- friends_server/src/main/kotlin/com/friends/profile/repository/ProfileRepository.kt
+++ friends_server/src/main/kotlin/com/friends/profile/repository/ProfileRepository.kt
@@ -2,7 +2,27 @@ package com.friends.profile.repository
 
 import com.friends.profile.entity.Profile
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ProfileRepository : JpaRepository<Profile, Long> {
     fun findByMemberId(memberId: Long): Profile?
+
+    @Query(
+        value = """
+            SELECT *
+            FROM Profile
+            WHERE ST_DWithin(
+                ST_Transform(geometry_point, 3857), 
+                ST_Transform(ST_SetSRID(ST_MakePoint(:lng, :lat), 4326), 3857),
+                :distance
+            )
+        """,
+        nativeQuery = true,
+    )
+    fun findNodesWithinDistance(
+        @Param("lng") lng: Double,
+        @Param("lat") lat: Double,
+        @Param("distance") distance: Double,
+    ): List<Profile>
 }
