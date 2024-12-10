296b6075cd8fd93cecd9d24a8420d3bd06bc4f76
Hosung <an3735297@naver.com>
2024-12-10 16:02:35 +0900

    feat : n 미터 이내의 프로필 조회 쿼리 작성

--- friends_server/src/main/kotlin/com/friends/profile/repository/ProfileRepository.kt
+++ friends_server/src/main/kotlin/com/friends/profile/repository/ProfileRepository.kt
@@ -1,8 +1,29 @@
 package com.friends.profile.repository
 
 import com.friends.profile.entity.Profile
+import io.lettuce.core.dynamic.annotation.Param
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface ProfileRepository : JpaRepository<Profile, Long> {
     fun findByMemberId(memberId: Long): Profile?
+
+    // distance 미터 이내의 프로필을 조회합니다.
+    @Query(
+        value = """
+            SELECT *
+            FROM Profile
+            WHERE ST_DWithin(
+                ST_Transform(geometry_point, 3857), 
+                ST_Transform(ST_SetSRID(ST_MakePoint(:lng, :lat), 4326), 3857),
+                :distance
+            )
+        """,
+        nativeQuery = true,
+    )
+    fun findNodesWithinDistance(
+        @Param("lng") lng: Double,
+        @Param("lat") lat: Double,
+        @Param("distance") distance: Double,
+    ): List<Profile>
 }
