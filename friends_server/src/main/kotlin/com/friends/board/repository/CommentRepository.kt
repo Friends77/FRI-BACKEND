fc18870c03eaf56320dabb204d7dba2ce9854b3d
djsumin <judy3504@gmail.com>
2024-12-11 23:36:58 +0900

    feat: 시간 순서에 따른 조회 및 20개 댓글 단위의 페이징 처리

--- friends_server/src/main/kotlin/com/friends/board/repository/CommentRepository.kt
+++ friends_server/src/main/kotlin/com/friends/board/repository/CommentRepository.kt
@@ -2,7 +2,10 @@ package com.friends.board.repository
 
 import com.friends.board.entity.Comment
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
 
 interface CommentRepository : JpaRepository<Comment, Long> {
     fun findByBoardIdAndId(boardId: Long, id: Long): Comment?
+    fun findTop20ByBoardIdOrderByCreatedAtAsc(boardId: Long): List<Comment>
+    fun findTop20ByBoardIdAndCreatedAtAfterOrderByCreatedAtAsc(boardId: Long, createdAt: LocalDateTime): List<Comment>
 }
--- friends_server/src/main/kotlin/com/friends/board/service/CommentQueryService.kt
+++ friends_server/src/main/kotlin/com/friends/board/service/CommentQueryService.kt
@@ -3,21 +3,31 @@ package com.friends.board.service
 import com.friends.board.entity.Comment
 import com.friends.board.exception.BoardNotFoundException
 import com.friends.board.repository.BoardRepository
+import com.friends.board.repository.CommentRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Service
-class CommentQueryService (
+class CommentQueryService(
     private val boardRepository: BoardRepository,
+    private val commentRepository: CommentRepository,
 ){
     //댓글조회
     @Transactional(readOnly = true)
     fun getCommentList(
         boardId: Long,
+        lastCreatedAt: LocalDateTime? = null,
     ): List<Comment> {
-        val board = boardRepository.findById(boardId).orElseThrow{
+        if(!boardRepository.existsById(boardId)) {
             throw BoardNotFoundException()
         }
-        return board.comments.sortedBy { it.createdAt }
+        return if(lastCreatedAt == null){
+            commentRepository.findTop20ByBoardIdOrderByCreatedAtAsc(boardId)
+        } else {
+            commentRepository.findTop20ByBoardIdAndCreatedAtAfterOrderByCreatedAtAsc(
+                boardId, lastCreatedAt
+            )
+        }
     }
 }
