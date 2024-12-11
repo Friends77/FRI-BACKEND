31dc478386c2e43e3e9713e6c377ab94f4f232cc
Chae <oumuamua121@gmail.com>
2024-12-11 18:44:35 +0900

    Feat : required = false 지우기

--- friends_server/src/main/kotlin/com/friends/chat/dto/ChatRoomRequestDto.kt
+++ friends_server/src/main/kotlin/com/friends/chat/dto/ChatRoomRequestDto.kt
@@ -11,6 +11,6 @@ data class ChatRoomCreateRequestDto(
     @field:Length(min = 1, max = 20, message = "채팅방 제목은 1자 이상 20자 이하로 입력해주세요.") //TODO: 제목 최대 길이 제한
     val title: String,
     @field:Size(min = 1, message = "채팅방 카테고리는 최소 1개 이상 선택해주세요.") //TODO: 카테고리 최대 개수 제한
-    @Schema(description = "채팅방 카테고리 ID 리스트", required = false)
+    @Schema(description = "채팅방 카테고리 ID 리스트")
     val categoryIdList: Set<Long>,
 )
