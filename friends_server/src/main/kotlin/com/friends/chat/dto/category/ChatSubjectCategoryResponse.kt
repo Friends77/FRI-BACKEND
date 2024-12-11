a38ae67f3dfa18b0b2612f667290fffee2eca270
Chae <oumuamua121@gmail.com>
2024-12-11 18:55:30 +0900

    Feat : 파일명 변경 및 필드 설명 추가

--- friends_server/src/main/kotlin/com/friends/chat/dto/category/ChatSubjectCategoryResponse.kt
+++ friends_server/src/main/kotlin/com/friends/chat/dto/category/ChatSubjectCategoryResponse.kt
@@ -1,6 +1,10 @@
 package com.friends.chat.dto.category
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class ChatSubjectInfoResponse(
+    @Schema(description = "채팅방 주제 카테고리 ID")
     val id: Long,
+    @Schema(description = "채팅방 주제 카테고리 이름")
     val name: String,
 )
