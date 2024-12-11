ad060c5a70998047d0c446fa785fe07500f9ac37
Chae <oumuamua121@gmail.com>
2024-12-11 16:45:51 +0900

    Feat : API 경로 변경

--- friends_server/src/main/kotlin/com/friends/chat/controller/ChatRoomController.kt
+++ friends_server/src/main/kotlin/com/friends/chat/controller/ChatRoomController.kt
@@ -8,13 +8,14 @@ import com.friends.common.dto.SliceBaseResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-@RequestMapping("/api/user/chat")
+@RequestMapping("/api/user/chat/room")
 class ChatRoomController(
     private val chatRoomCommandService: ChatRoomCommandService,
     private val chatRoomQueryService: ChatRoomQueryService,
@@ -29,6 +30,7 @@ class ChatRoomController(
         return ResponseEntity.status(HttpStatus.CREATED).build()
     }
 
+    @GetMapping
     override fun getChatRooms(
         memberId: Long,
         size: Int,
--- friends_server/src/main/kotlin/com/friends/chat/dto/ChatRoomRequestDto.kt
+++ friends_server/src/main/kotlin/com/friends/chat/dto/ChatRoomRequestDto.kt
@@ -1,13 +1,16 @@
 package com.friends.chat.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 import org.hibernate.validator.constraints.Length
 
 data class ChatRoomCreateRequestDto(
+    @Schema(description = "채팅방 제목")
     @field:NotBlank(message = "채팅방 제목은 공백일 수 없습니다.")
     @field:Length(min = 1, max = 20, message = "채팅방 제목은 1자 이상 20자 이하로 입력해주세요.") //TODO: 제목 최대 길이 제한
     val title: String,
     @field:Size(min = 1, message = "채팅방 카테고리는 최소 1개 이상 선택해주세요.") //TODO: 카테고리 최대 개수 제한
+    @Schema(description = "채팅방 카테고리 ID 리스트", required = false)
     val categoryIdList: Set<Long>,
 )
