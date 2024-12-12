589602318fa119ad820ad0059bc9875cfbd53d04
Hosung <an3735297@naver.com>
2024-12-12 22:51:57 +0900

    feat : profile 테스트 재작성

--- friends_server/src/test/kotlin/com/friends/profile/repository/PostgisTest.kt
+++ friends_server/src/test/kotlin/com/friends/profile/repository/PostgisTest.kt
@@ -1,77 +1,60 @@
 package com.friends.profile.repository
 
+import com.friends.member.entity.Member
+import com.friends.member.repository.MemberRepository
 import com.friends.profile.dto.LocationDto
+import com.friends.profile.entity.GenderEnum
+import com.friends.profile.entity.Profile
 import com.friends.support.annotation.RepositoryTest
-import io.kotest.matchers.collections.shouldContainExactly
-import jakarta.persistence.EntityManager
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import java.time.LocalDate
 
 @RepositoryTest
-class PostgisTest(
-    @Autowired private val testPointRepository: TestPointRepository,
-    @Autowired private val entityManager: EntityManager,
-) {
-    @Test
-    fun `5,000 미터 이내의 TestPoint를 검색할 때, 5,000 미터 이내의 TestPoint만 반환해야 합니다`() {
-        // given
-        val testPoint1 = TestPoint.of(LocationDto(longitude = 10.0, latitude = 10.0))
-        val testPoint2 = TestPoint.of(LocationDto(longitude = 10.1, latitude = 10.1))
-        val testPoint3 = TestPoint.of(LocationDto(longitude = 20.0, latitude = 20.0))
+class ProfileRepositoryTest
+    @Autowired
+    constructor(
+        private val profileRepository: ProfileRepository,
+        private val memberRepository: MemberRepository,
+    ) {
+        private lateinit var profile1: Profile
+        private lateinit var profile2: Profile
+        private lateinit var profile3: Profile
 
-        val result1 = testPointRepository.save(testPoint1)
-        testPointRepository.save(testPoint2)
-        testPointRepository.save(testPoint3)
+        @BeforeEach
+        fun setup() {
+            val testPoint1 = LocationDto(10.0, 10.0)
+            val testPoint2 = LocationDto(10.1, 10.1)
+            val testPoint3 = LocationDto(20.0, 20.0)
 
-        entityManager.flush()
-        entityManager.clear()
+            val member1 = memberRepository.save(Member.createUser("test1", "test1@com"))
+            val member2 = memberRepository.save(Member.createUser("test2", "test2@com"))
+            val member3 = memberRepository.save(Member.createUser("test3", "test3@com"))
 
-        // when
-        val foundPoints = testPointRepository.findNodesWithinDistance(10.0, 10.0, 5000.0)
+            profile1 = profileRepository.save(Profile.of(member1, birth = LocalDate.now(), gender = GenderEnum.MAN, location = testPoint1, interestTag = mutableSetOf("일상"), imageUrl = "test imageurl"))
+            profile2 = profileRepository.save(Profile.of(member2, birth = LocalDate.now(), gender = GenderEnum.MAN, location = testPoint2, interestTag = mutableSetOf("일상"), imageUrl = "test imageurl"))
+            profile3 = profileRepository.save(Profile.of(member3, birth = LocalDate.now(), gender = GenderEnum.MAN, location = testPoint3, interestTag = mutableSetOf("일상"), imageUrl = "test imageurl"))
+        }
 
-        // then
-        foundPoints.map { it.id } shouldContainExactly listOf(result1.id)
+        @Test
+        fun `5000 미터 이내의 TestPoint 검색`() {
+            // (10,10) (10.1,10.1) 사이의 거리는 약 15,000 m 입니다
+            val foundPoints = profileRepository.findNodesWithinDistance(10.0, 10.0, 5000.0)
+            assertEquals(listOf(profile1.id), foundPoints.map { it.id })
+        }
+
+        @Test
+        fun `20000 미터 이내의 TestPoint 검색`() {
+            // (10,10) (20,20) 사이의 거리는 10,000,000 m 이상입니다
+            val foundPoints = profileRepository.findNodesWithinDistance(10.0, 10.0, 20000.0)
+            assertEquals(listOf(profile1.id, profile2.id), foundPoints.map { it.id })
+        }
+
+        @Test
+        fun `20000000 미터 이내의 TestPoint 검색`() {
+            val foundPoints = profileRepository.findNodesWithinDistance(10.0, 10.0, 20000000.0)
+            assertEquals(listOf(profile1.id, profile2.id, profile3.id), foundPoints.map { it.id })
+        }
     }
-
-    @Test
-    fun `20,000 미터 이내의 TestPoint를 검색할 때, 20,000 미터 이내의 TestPoint만 반환해야 합니다`() {
-        // given
-        val testPoint1 = TestPoint.of(LocationDto(longitude = 10.0, latitude = 10.0))
-        val testPoint2 = TestPoint.of(LocationDto(longitude = 10.1, latitude = 10.1))
-        val testPoint3 = TestPoint.of(LocationDto(longitude = 20.0, latitude = 20.0))
-
-        val result1 = testPointRepository.save(testPoint1)
-        val result2 = testPointRepository.save(testPoint2)
-        testPointRepository.save(testPoint3)
-
-        entityManager.flush()
-        entityManager.clear()
-
-        // when
-        val foundPoints = testPointRepository.findNodesWithinDistance(10.0, 10.0, 20000.0)
-
-        // then
-        foundPoints.map { it.id } shouldContainExactly listOf(result1.id, result2.id)
-    }
-
-    @Test
-    fun `20,000,000 미터 이내의 TestPoint를 검색할 때, 모든 TestPoint를 반환해야 합니다`() {
-        // given
-        val testPoint1 = TestPoint.of(LocationDto(longitude = 10.0, latitude = 10.0))
-        val testPoint2 = TestPoint.of(LocationDto(longitude = 10.1, latitude = 10.1))
-        val testPoint3 = TestPoint.of(LocationDto(longitude = 20.0, latitude = 20.0))
-
-        val result1 = testPointRepository.save(testPoint1)
-        val result2 = testPointRepository.save(testPoint2)
-        val result3 = testPointRepository.save(testPoint3)
-
-        entityManager.flush()
-        entityManager.clear()
-
-        // when
-        val foundPoints = testPointRepository.findNodesWithinDistance(10.0, 10.0, 20000000.0)
-
-        // then
-        foundPoints.map { it.id } shouldContainExactly listOf(result1.id, result2.id, result3.id)
-    }
-}
