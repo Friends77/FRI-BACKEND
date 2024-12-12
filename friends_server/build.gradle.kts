3f028be6806aa68316e984d3e84c68076f9cf198
HOSUNG <an3735297@pusan.ac.kr>
2024-12-12 21:41:31 +0900

    feat : flyway 적용

--- friends_server/build.gradle.kts
+++ friends_server/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     implementation("org.locationtech.jts:jts-core:1.19.0")
     implementation("org.hibernate.orm:hibernate-spatial:6.4.4.Final")
 
+    // flyway
+    implementation("org.flywaydb:flyway-core")
+
     // Test
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
