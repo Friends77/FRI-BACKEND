1880354c694d06b6e950c033c12f476a78238a06
Hosung <an3735297@naver.com>
2024-12-13 13:46:51 +0900

    feat : dto projection

--- friends_server/src/main/kotlin/com/friends/profile/repository/ProfileRepository.kt
+++ friends_server/src/main/kotlin/com/friends/profile/repository/ProfileRepository.kt
@@ -11,21 +11,19 @@ interface ProfileRepository : JpaRepository<Profile, Long> {
 
     // Haversine 공식을 사용해서 주어진 좌표에서 주어진 거리 이내에 있는 프로필을 찾는다.
     @Query(
-        value = """
-        SELECT 
-            p.profile_id AS id,
-            m.nickname AS nickname,
-            p.image_url AS imageUrl,
-            (6371000 * ACOS(COS(RADIANS(:latitude)) * COS(RADIANS(p.location_latitude)) * 
-            COS(RADIANS(p.location_longitude) - RADIANS(:longitude)) + 
-            SIN(RADIANS(:latitude)) * SIN(RADIANS(p.location_latitude)))) AS distance
-        FROM profile p
-        INNER JOIN member m ON p.member_id = m.member_id
-        WHERE (6371000 * ACOS(COS(RADIANS(:latitude)) * COS(RADIANS(p.location_latitude)) * 
-              COS(RADIANS(p.location_longitude) - RADIANS(:longitude)) + 
-              SIN(RADIANS(:latitude)) * SIN(RADIANS(p.location_latitude)))) <= :distance
+        """
+        SELECT new com.friends.profile.dto.ProfileWithDistanceDto(
+            p.id, m.nickname, p.imageUrl,
+            (6371000 * ACOS(COS(RADIANS(:latitude)) * COS(RADIANS(p.location.latitude)) * 
+            COS(RADIANS(p.location.longitude) - RADIANS(:longitude)) + 
+            SIN(RADIANS(:latitude)) * SIN(RADIANS(p.location.latitude))))
+        )
+        FROM Profile p
+        INNER JOIN Member m ON p.id = m.id
+        WHERE (6371000 * ACOS(COS(RADIANS(:latitude)) * COS(RADIANS(p.location.latitude)) * 
+              COS(RADIANS(p.location.longitude) - RADIANS(:longitude)) + 
+              SIN(RADIANS(:latitude)) * SIN(RADIANS(p.location.latitude)))) <= :distance
     """,
-        nativeQuery = true,
     )
     fun findWithInDistance(
         @Param("latitude") latitude: Double,
