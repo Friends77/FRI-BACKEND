8009b4e43e45216647cccbfc98244cef4db7a614
Hosung <an3735297@naver.com>
2024-12-18 20:32:38 +0900

    feat : build.gradle 의존성 추가 (websocket)

--- friends_server/build.gradle.kts
+++ friends_server/build.gradle.kts
@@ -63,6 +63,9 @@ dependencies {
     // aws
     implementation("com.amazonaws:aws-java-sdk-s3:1.12.375")
 
+    // websocket
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+
     // Test
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
