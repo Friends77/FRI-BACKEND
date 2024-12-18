a9646f5d98d99b72d2bc25bd5460bdf0440bdb62
djsumin <judy3504@gmail.com>
2024-12-18 23:06:21 +0900

    refactor: repostiory null 체크

--- friends_server/src/main/kotlin/com/friends/board/repository/LikeRepository.kt
+++ friends_server/src/main/kotlin/com/friends/board/repository/LikeRepository.kt
@@ -6,5 +6,5 @@ import com.friends.member.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface LikeRepository : JpaRepository<Like, Long> {
-    fun findByMemberAndBoard(member: Member?, board: Board?): Like
+    fun findByMemberAndBoard(member: Member, board: Board): Like?
 }
