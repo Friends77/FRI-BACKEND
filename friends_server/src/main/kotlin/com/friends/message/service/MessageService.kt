76ea01c0727b207c4132b49147b3a73e1e866b15
Hosung <an3735297@naver.com>
2024-12-21 11:26:59 +0900

    feat : message 전송 시 DB 에 저장

--- friends_server/src/main/kotlin/com/friends/message/service/MessageService.kt
+++ friends_server/src/main/kotlin/com/friends/message/service/MessageService.kt
@@ -1,8 +1,14 @@
 package com.friends.message.service
 
+import com.friends.chat.ChatRoomNotFoundException
 import com.friends.chat.dto.ChatSendMessageDto
+import com.friends.chat.repository.ChatRoomRepository
 import com.friends.common.util.JsonUtil
+import com.friends.member.MemberNotFoundException
 import com.friends.member.repository.MemberRepository
+import com.friends.message.entity.Message
+import com.friends.message.entity.MessageType
+import com.friends.message.repository.MessageRepository
 import org.springframework.stereotype.Service
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
@@ -11,7 +17,9 @@ import java.util.concurrent.CopyOnWriteArraySet
 
 @Service
 class MessageService(
+    private val messageRepository: MessageRepository,
     private val memberRepository: MemberRepository,
+    private val chatRoomRepository: ChatRoomRepository,
 ) {
     // 채팅방 ID를 키로 하고, 각 채팅방의 세션을 Set으로 저장
     private val chatRooms: MutableMap<Long, MutableSet<WebSocketSession>> = ConcurrentHashMap()
@@ -34,11 +42,34 @@ class MessageService(
     fun sendMessage(
         chatRoomId: Long,
         message: ChatSendMessageDto,
+        type: MessageType,
     ) {
         val sessions = chatRooms[chatRoomId]
+
+        /**
+         * 메세지를 보낼 때마다 보낸 유저와 채팅방이 있는지 DB 에 확인합니다.
+         * 이 과정이 비효율적일 경우 아래 프록시 객체를 생성하여 메세지를 보내는 로직을 고려합니다.
+         * 프록시 객체는 DB 에서 채팅방과 유저 정보를 요청하지 않지만, DB 에 데이터가 있는지 없는지 확인할 수 없습니다.
+         *
+         * val chatRoom = entityManager.getReference(ChatRoom::class.java, chatRoomId)
+         * val sender = entityManager.getReference(Member::class.java, message.senderId)
+         */
+        val chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow { ChatRoomNotFoundException() }
+        val sender = memberRepository.findById(message.senderId).orElseThrow { MemberNotFoundException() }
+
+        val savedMessage = messageRepository.save(Message.of(chatRoom, sender, message.message, type))
+        val sendMessageDto =
+            ChatSendMessageDto(
+                senderId = sender.id,
+                senderName = sender.nickname,
+                senderProfileImageUrl = sender.imageUrl ?: "default image url", // TODO : member 와 profile 에 imageUrl 이 중복되어 있는 것을 확인 -> 둘 중 하나로 통일하고 non-null 로 변경 (회원가입 시 주어지지 않는다면 default image url 로 설정)
+                message = savedMessage.content,
+                sendTime = savedMessage.createdAt,
+                type = savedMessage.type,
+            )
         sessions?.forEach { session ->
             if (session.isOpen) {
-                session.sendMessage(TextMessage(JsonUtil.toJson(message)))
+                session.sendMessage(TextMessage(JsonUtil.toJson(sendMessageDto)))
             }
         }
     }
