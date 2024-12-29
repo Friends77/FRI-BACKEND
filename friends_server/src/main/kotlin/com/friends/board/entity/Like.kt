f4e0625f6f6d6e2a0c32004953b2eae97f01d7b6
Chae <oumuamua121@gmail.com>
2024-12-29 17:14:45 +0900

    DB 예약어에 따른 처리

--- friends_server/src/main/kotlin/com/friends/board/entity/Like.kt
+++ friends_server/src/main/kotlin/com/friends/board/entity/Like.kt
@@ -10,7 +10,7 @@ import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 
-@Entity
+@Entity(name = "\"like\"")
 class Like(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "like_id")
