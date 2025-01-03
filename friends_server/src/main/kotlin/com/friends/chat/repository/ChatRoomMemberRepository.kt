caec24604d4314c5ce07b3a0948ee40eb9091eb1
Hosung <an3735297@naver.com>
2025-01-03 12:06:59 +0900

    feat : 웹소켓 핸들러와 메세지 전송 서비스 분리

--- friends_server/src/main/kotlin/com/friends/chat/repository/ChatRoomMemberRepository.kt
+++ friends_server/src/main/kotlin/com/friends/chat/repository/ChatRoomMemberRepository.kt
@@ -28,6 +28,8 @@ interface ChatRoomMemberRepository :
         chatRoom: ChatRoom,
         member: Member,
     ): Boolean
+
+    fun findAllByMemberId(memberId: Long): List<ChatRoomMember>
 }
 
 interface ChatRoomMemberCustomRepository {
