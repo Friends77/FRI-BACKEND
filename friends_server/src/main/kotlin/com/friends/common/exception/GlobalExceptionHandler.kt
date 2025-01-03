e68cdb215cffb349c65972dfccdc8c360cc6d773
Chae <oumuamua121@gmail.com>
2025-01-03 23:23:04 +0900

    Feat : 에러 헨들러 추가 작성

--- friends_server/src/main/kotlin/com/friends/common/exception/GlobalExceptionHandler.kt
+++ friends_server/src/main/kotlin/com/friends/common/exception/GlobalExceptionHandler.kt
@@ -1,5 +1,7 @@
 package com.friends.common.exception
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import com.friends.board.BoardException
 import com.friends.board.exception.CommentException
 import com.friends.chat.ChatException
@@ -8,20 +10,113 @@ import com.friends.member.MemberExceptions
 import com.friends.oauth2.OAuth2Exception
 import com.friends.profile.ProfileExceptions
 import com.friends.security.securityException.AuthenticationException
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpMediaTypeNotSupportedException
+import org.springframework.web.HttpRequestMethodNotSupportedException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.MissingPathVariableException
+import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.method.annotation.HandlerMethodValidationException
+import org.springframework.web.multipart.support.MissingServletRequestPartException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice // @ControllerAdvice와 @ResponseBody를 결합한 것
 class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
-    private val log: Logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+    override fun handleMethodArgumentNotValid(//메소드 인자 유효성 검사 실패 시 발생하는 예외 처리
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? {
+        logger.error(ex)
+        return getInvalidRequestResponse(ex.messages().joinToString())
+    }
+
+    override fun handleHttpMessageNotReadable( //JSON 요청이 잘못된 경우 발생하는 예외 처리(예: JSON 형식이 잘못된 경우)
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? {
+        val errorMessage =
+            when (val cause = ex.cause) {
+                is InvalidFormatException -> "${cause.path.joinToString(separator = ".") { it?.fieldName.orEmpty() }}: ${ex.message}" // 유효성 검사 실패 시 발생하는 예외 처리(예: JSON 형식이 잘못된 경우)
+                is MismatchedInputException -> {
+                    "${cause.path.joinToString(separator = ".") { it?.fieldName.orEmpty() }}: ${ex.message}" // 입력 값이 예상과 다른 경우 발생하는 예외 처리(예: 문자열을 숫자로 변환할 수 없는 경우)
+                }
+                else -> "유효하지 않은 요청입니다"
+            }
+        logger.error(ex)
+        return getInvalidRequestResponse(errorMessage)
+    }
+
+    override fun handleHttpRequestMethodNotSupported( //지원되지 않는 HTTP 메소드 요청 시 발생하는 예외 처리 (예: GET 요청을 POST로 보낼 때)
+        ex: HttpRequestMethodNotSupportedException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? {
+        logger.error(ex)
+        return getInvalidRequestResponse(ex.message)
+    }
+
+    override fun handleMissingServletRequestPart( //필수 요청 파라미터 누락 시 발생하는 예외 처리(예: required = true로 설정된 파라미터가 없는 경우)
+        ex: MissingServletRequestPartException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? {
+        logger.error(ex)
+        return getInvalidRequestResponse(ex.message)
+    }
+
+    override fun handleMissingServletRequestParameter( //multipart/form-data 요청 필수 부분 누락 시 발생하는 예외 처리
+        ex: MissingServletRequestParameterException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? {
+        logger.error(ex)
+        return getInvalidRequestResponse(ex.message)
+    }
+
+    override fun handleMissingPathVariable(//필수 경로 변수 누락 시 발생하는 예외 처리
+        ex: MissingPathVariableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? {
+        logger.error(ex)
+        return getInvalidRequestResponse(ex.message)
+    }
+
+    override fun handleHttpMediaTypeNotSupported(// 서버가 지원하지 않는 미디어 타입 요청 시 발생하는 예외 처리
+        ex: HttpMediaTypeNotSupportedException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? {
+        logger.error(ex)
+        return getInvalidRequestResponse(ex.message, HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    }
+
+    override fun handleHandlerMethodValidationException(//@Valid 유효성 검사 실패 시 발생하는 예외 처리
+        ex: HandlerMethodValidationException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest,
+    ): ResponseEntity<Any>? = getInvalidRequestResponse(ex.messages().joinToString())
 
     @ExceptionHandler(AuthenticationException::class)
     fun handleInvalidJwtException(ex: AuthenticationException): ResponseEntity<Any> {
-        log.error("Invalid JWT", ex)
+        logger.error("Invalid JWT", ex)
         return ResponseEntity
             .status(ex.errorCode.httpStatus)
             .body(ErrorResponse.of(ex.errorCode, ex.message))
@@ -29,7 +124,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(EmailException::class)
     fun handleEmailException(ex: EmailException): ResponseEntity<Any> {
-        log.error("Email Exception", ex)
+        logger.error("Email Exception", ex)
         return ResponseEntity
             .status(ex.errorCode.httpStatus)
             .body(ErrorResponse.of(ex.errorCode, ex.message))
@@ -37,7 +132,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(OAuth2Exception::class)
     fun handleOAuth2Exception(ex: OAuth2Exception): ResponseEntity<Any> {
-        log.error("OAuth2 Exception", ex)
+        logger.error("OAuth2 Exception", ex)
         return ResponseEntity
             .status(ex.errorCode.httpStatus)
             .body(ErrorResponse.of(ex.errorCode, ex.message))
@@ -45,7 +140,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(BoardException::class)
     fun handleBoardException(ex: BoardException): ResponseEntity<Any> {
-        log.error("Board Exception", ex)
+        logger.error("Board Exception", ex)
         return ResponseEntity
             .status(ex.errorCode.httpStatus)
             .body(ErrorResponse.of(ex.errorCode, ex.message))
@@ -53,7 +148,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(ProfileExceptions::class)
     fun handleProfileException(ex: ProfileExceptions): ResponseEntity<Any> {
-        log.error("Profile Exception", ex)
+        logger.error("Profile Exception", ex)
         return ResponseEntity
             .status(ex.errorCode.httpStatus)
             .body(ErrorResponse.of(ex.errorCode, ex.message))
@@ -61,7 +156,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(MemberExceptions::class)
     fun handleMemberException(ex: MemberExceptions): ResponseEntity<Any> {
-        log.error("Member Exception", ex)
+        logger.error("Member Exception", ex)
         return ResponseEntity
             .status(ex.errorCode.httpStatus)
             .body(ErrorResponse.of(ex.errorCode, ex.message))
@@ -69,7 +164,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(ChatException::class)
     fun handleChatException(ex: ChatException): ResponseEntity<Any> {
-        log.error("Chat Exception", ex)
+        logger.error("Chat Exception", ex)
         return ResponseEntity
             .status(ex.errorCode.httpStatus)
             .body(ErrorResponse.of(ex.errorCode, ex.message))
@@ -77,17 +172,26 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(CommentException::class)
     fun handleCommentException(ex: CommentException): ResponseEntity<Any> {
-        log.error("Comment Exception", ex)
+        logger.error("Comment Exception", ex)
         return ResponseEntity
             .status(ex.errorCode.httpStatus)
             .body(ErrorResponse.of(ex.errorCode, ex.message))
     }
 
-//    @ExceptionHandler(Exception::class)
-//    fun handleException(ex: Exception): ResponseEntity<Any> {
-//        log.error("Exception", ex)
-//        return ResponseEntity
-//            .status(ex.httpStatus)
-//            .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, ex.message))
-//    }
+    private fun MethodArgumentNotValidException.messages(): List<String> = bindingResult.fieldErrors.map { "${it.field}: ${it.defaultMessage.orEmpty()}" }
+
+    private fun HandlerMethodValidationException.messages(): List<String> =
+        this.allErrors.map {
+            it.defaultMessage.orEmpty()
+        }
+
+    private fun getInvalidRequestResponse(
+        errorMessage: String?,
+        httpStatus: HttpStatus = HttpStatus.BAD_REQUEST,
+    ): ResponseEntity<Any> {
+        val invalidRequestErrorCode = ErrorCode.INVALID_REQUEST
+        return ResponseEntity
+            .status(httpStatus)
+            .body(ErrorResponse.of(invalidRequestErrorCode, errorMessage))
+    }
 }
