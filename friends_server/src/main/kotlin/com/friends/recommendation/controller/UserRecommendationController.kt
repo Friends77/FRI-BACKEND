8f65043ba316621eb9d061f5f49f84608683503a
Hosung <an3735297@naver.com>
2025-01-06 16:39:06 +0900

    fix : 거리기반 추천을 user 권한으로 변경

--- friends_server/src/main/kotlin/com/friends/recommendation/controller/UserRecommendationController.kt
+++ friends_server/src/main/kotlin/com/friends/recommendation/controller/UserRecommendationController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/global/recommendation")
+@RequestMapping("/api/user/recommendation")
 class UserRecommendationController(
     private val userRecommendationService: UserRecommendationService,
 ) : UserRecommendationControllerSpec {
