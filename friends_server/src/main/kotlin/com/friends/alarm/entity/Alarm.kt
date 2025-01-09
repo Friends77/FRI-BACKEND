0bac92a4506d0850f1e771825a65ef974645e18f
Hosung <an3735297@naver.com>
2025-01-09 22:41:01 +0900

    feat : 알람 앤티티 설계

---
+++ friends_server/src/main/kotlin/com/friends/alarm/entity/Alarm.kt
@@ -0,0 +1,22 @@
+package com.friends.alarm.entity
+
+import com.friends.common.entity.BaseModifiableEntity
+import com.friends.member.entity.Member
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity
+class Alarm(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    val member: Member,
+    val type: AlarmType,
+    val message: String,
+) : BaseModifiableEntity()
