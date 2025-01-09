a293d785e176d2ee1453b9644d5c4d632bd1284e
Hosung <an3735297@naver.com>
2025-01-09 14:10:13 +0900

    fix : 존재하지 않는 카테고리 시 적절한 에러 반환

--- friends_server/src/main/kotlin/com/friends/common/exception/ErrorCode.kt
+++ friends_server/src/main/kotlin/com/friends/common/exception/ErrorCode.kt
@@ -68,4 +68,7 @@ enum class ErrorCode(
     // Comment API error 18000대
     NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, -18001, "존재하지 않는 댓글입니다."),
     INVALID_COMMENT_ACCESS(HttpStatus.FORBIDDEN, -18002, "댓글에 대한 유효하지 않은 접근입니다."),
+
+    // category API error 19000대
+    NOT_FOUND_CATEGORY(HttpStatus.NOT_FOUND, -19001, "존재하지 않는 카테고리입니다."),
 }
--- friends_server/src/main/kotlin/com/friends/security/service/AuthService.kt
+++ friends_server/src/main/kotlin/com/friends/security/service/AuthService.kt
@@ -1,5 +1,6 @@
 package com.friends.security.service
 
+import com.friends.category.CategoryNotFoundException
 import com.friends.category.repository.CategoryRepository
 import com.friends.config.AuthProperties
 import com.friends.jwt.AtRtService
@@ -119,7 +120,7 @@ class AuthService(
         // 프로필 관심사 태그 생성
         profileInterestTagRepository.saveAll(
             registerRequestDto.interestTag.map {
-                ProfileInterestTag(profile = profile, category = categoryRepository.findById(it).orElseThrow { IllegalArgumentException() })
+                ProfileInterestTag(profile = profile, category = categoryRepository.findById(it).orElseThrow { CategoryNotFoundException() })
             },
         )
     }
