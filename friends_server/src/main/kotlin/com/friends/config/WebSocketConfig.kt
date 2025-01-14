052f864d48b5f0ecf8142d8429e10207be6e46f7
Hosung <an3735297@naver.com>
2025-01-14 14:11:16 +0900

    refactor : 클래스 명 변경

--- friends_server/src/main/kotlin/com/friends/config/WebSocketConfig.kt
+++ friends_server/src/main/kotlin/com/friends/config/WebSocketConfig.kt
@@ -1,9 +1,8 @@
 package com.friends.config
 
 import com.friends.alarm.websocket.AlarmWebsocketHandler
-import com.friends.alarm.websocket.AlarmWebsocketInterceptor
+import com.friends.chat.websocket.AuthWebsocketInterceptor
 import com.friends.chat.websocket.ChatWebSocketHandler
-import com.friends.chat.websocket.ChatWebsocketInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
@@ -13,19 +12,18 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 class WebSocketConfig(
     private val chatWebSocketHandler: ChatWebSocketHandler,
-    private val chatWebsocketInterceptor: ChatWebsocketInterceptor,
+    private val authWebsocketInterceptor: AuthWebsocketInterceptor,
     private val alarmWebsocketHandler: AlarmWebsocketHandler,
-    private val alarmWebsocketInterceptor: AlarmWebsocketInterceptor,
 ) : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry
             .addHandler(chatWebSocketHandler, "/ws/chat")
-            .addInterceptors(chatWebsocketInterceptor)
+            .addInterceptors(authWebsocketInterceptor)
             .setAllowedOrigins("*") // CORS 허용
 
         registry
             .addHandler(alarmWebsocketHandler, "/ws/alarm")
-            .addInterceptors(alarmWebsocketInterceptor)
+            .addInterceptors(authWebsocketInterceptor)
             .setAllowedOrigins("*") // CORS 허용
     }
 }
