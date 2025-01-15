706fbfd691ea20eaf64ecb3a9bad81990927400b
Hosung <an3735297@naver.com>
2025-01-15 17:02:19 +0900

    feat : DB 에 Enum 타입이 int 가 아닌 string 형식으로 저장되도록 변경

--- friends_server/src/main/kotlin/com/friends/friendship/entity/Friendship.kt
+++ friends_server/src/main/kotlin/com/friends/friendship/entity/Friendship.kt
@@ -4,6 +4,8 @@ import com.friends.common.entity.BaseModifiableEntity
 import com.friends.member.entity.Member
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -24,6 +26,7 @@ class Friendship(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "receive_member_id")
     val receiver: Member,
+    @Enumerated(EnumType.STRING)
     private var friendshipStatus: FriendshipStatusEnums,
 ) : BaseModifiableEntity() {
     fun getFriendshipStatus(): FriendshipStatusEnums = friendshipStatus
