d591276b25b6623bca769ed6af12c37bfbc7622a
Hosung <an3735297@naver.com>
2025-01-15 19:24:31 +0900

    test : 친구 조회 통합 테스트

--- friends_server/src/main/kotlin/com/friends/friendship/service/FriendShipQueryService.kt
+++ friends_server/src/main/kotlin/com/friends/friendship/service/FriendShipQueryService.kt
@@ -2,7 +2,6 @@ package com.friends.friendship.service
 
 import com.friends.friendship.repository.FriendShipRepository
 import com.friends.profile.dto.ProfileSimpleResponseDto
-import com.friends.profile.repository.ProfileRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,7 +10,6 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class FriendShipQueryService(
     private val friendShipRepository: FriendShipRepository,
-    private val profileRepository: ProfileRepository,
 ) {
     @Value("\${image.profile-base-url}")
     lateinit var profileBaseImageUrl: String
@@ -22,28 +20,25 @@ class FriendShipQueryService(
         // Friendship 조회
         val friendships = friendShipRepository.findAllFriendsByMemberId(memberId)
 
-        // 친구의 memberId만 추출
-        val friendIds =
+        // 친구만 추출
+        val friends =
             friendships.map { friendship ->
                 // 요청자와 수락자 중 "나"가 아닌 멤버의 ID를 추출
                 if (friendship.requester.id == memberId) {
-                    friendship.receiver.id
+                    friendship.receiver
                 } else {
-                    friendship.requester.id
+                    friendship.requester
                 }
             }
 
-        // Profile 조회
-        val profiles = profileRepository.findAllById(friendIds)
-
         // DTO 변환 및 정렬
-        return profiles
-            .map { profile ->
+        return friends
+            .map { friend ->
                 ProfileSimpleResponseDto(
-                    memberId = profile.member.id,
-                    nickname = profile.member.nickname,
-                    imageUrl = profile.imageUrl ?: profileBaseImageUrl,
-                    selfDescription = profile.selfDescription,
+                    memberId = friend.id,
+                    nickname = friend.nickname,
+                    imageUrl = friend.profile?.imageUrl ?: profileBaseImageUrl,
+                    selfDescription = friend.profile?.selfDescription,
                 )
             }.sortedBy { it.nickname }
     }
