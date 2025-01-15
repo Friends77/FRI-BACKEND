c5b9bc9a137e93b546a8223deef20a96f7ea8e58
Hosung <an3735297@naver.com>
2025-01-15 17:02:34 +0900

    feat : 친구 조회 서비스

--- friends_server/src/main/kotlin/com/friends/friendship/repository/FriendShipRepository.kt
+++ friends_server/src/main/kotlin/com/friends/friendship/repository/FriendShipRepository.kt
@@ -1,6 +1,8 @@
 package com.friends.friendship.repository
 
+import com.friends.common.util.getList
 import com.friends.friendship.entity.Friendship
+import com.friends.friendship.entity.FriendshipStatusEnums
 import com.friends.member.entity.Member
 import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
 import org.springframework.data.jpa.repository.JpaRepository
@@ -19,8 +21,28 @@ interface FriendShipRepository :
     ): Friendship?
 }
 
-interface FriendShipCustomRepository
+interface FriendShipCustomRepository {
+    fun findAllFriendsByMemberId(
+        memberId: Long,
+    ): List<Friendship>
+}
 
 class FriendShipCustomRepositoryImpl(
     private val kotlinJdslJpqlExecutor: KotlinJdslJpqlExecutor,
-) : FriendShipCustomRepository
+) : FriendShipCustomRepository {
+    override fun findAllFriendsByMemberId(memberId: Long): List<Friendship> =
+        kotlinJdslJpqlExecutor
+            .getList {
+                select(entity(Friendship::class))
+                    .from(entity(Friendship::class))
+                    .where(
+                        and(
+                            or(
+                                path(Friendship::requester).path(Member::id).equal(memberId),
+                                path(Friendship::receiver).path(Member::id).equal(memberId),
+                            ),
+                            path(Friendship::getFriendshipStatus).equal(FriendshipStatusEnums.ACCEPT),
+                        ),
+                    )
+            }
+}
--- friends_server/src/main/kotlin/com/friends/friendship/service/FriendShipQueryService.kt
+++ friends_server/src/main/kotlin/com/friends/friendship/service/FriendShipQueryService.kt
@@ -1,7 +1,9 @@
 package com.friends.friendship.service
 
 import com.friends.friendship.repository.FriendShipRepository
+import com.friends.profile.dto.ProfileSimpleResponseDto
 import com.friends.profile.repository.ProfileRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -11,10 +13,38 @@ class FriendShipQueryService(
     private val friendShipRepository: FriendShipRepository,
     private val profileRepository: ProfileRepository,
 ) {
+    @Value("\${image.profile-base-url}")
+    lateinit var profileBaseImageUrl: String
+
     fun getFriendshipList(
         memberId: Long,
-        lastFriendShipId: Long,
-        size: Int,
-    ) {
+    ): List<ProfileSimpleResponseDto> {
+        // Friendship 조회
+        val friendships = friendShipRepository.findAllFriendsByMemberId(memberId)
+
+        // 친구의 memberId만 추출
+        val friendIds =
+            friendships.map { friendship ->
+                // 요청자와 수락자 중 "나"가 아닌 멤버의 ID를 추출
+                if (friendship.requester.id == memberId) {
+                    friendship.receiver.id
+                } else {
+                    friendship.requester.id
+                }
+            }
+
+        // Profile 조회
+        val profiles = profileRepository.findAllById(friendIds)
+
+        // DTO 변환 및 정렬
+        return profiles
+            .map { profile ->
+                ProfileSimpleResponseDto(
+                    memberId = profile.member.id,
+                    nickname = profile.member.nickname,
+                    imageUrl = profile.imageUrl ?: profileBaseImageUrl,
+                    selfDescription = profile.selfDescription,
+                )
+            }.sortedBy { it.nickname }
     }
 }
