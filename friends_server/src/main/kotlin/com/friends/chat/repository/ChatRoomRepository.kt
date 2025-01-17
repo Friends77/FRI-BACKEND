df4bc00f57612356a6c3c9ff4ce2e51bcc9cdd3a
Hosung <an3735297@naver.com>
2025-01-17 17:44:17 +0900

    feat : 채팅방 카테고리 추천 레파지토리 메서드

--- friends_server/src/main/kotlin/com/friends/chat/repository/ChatRoomRepository.kt
+++ friends_server/src/main/kotlin/com/friends/chat/repository/ChatRoomRepository.kt
@@ -1,15 +1,44 @@
 package com.friends.chat.repository
 
+import com.friends.category.entity.Category
 import com.friends.chat.entity.ChatRoom
+import com.friends.chat.entity.ChatRoomCategory
+import com.friends.common.util.getSlice
+import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
 import jakarta.persistence.LockModeType
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
-import org.springframework.stereotype.Repository
 
-@Repository
-interface ChatRoomRepository : JpaRepository<ChatRoom, Long> {
+interface ChatRoomRepository :
+    JpaRepository<ChatRoom, Long>,
+    ChatRoomCustomRepository {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT c FROM ChatRoom c WHERE c.id = :id")
     fun findByIdWithLock(id: Long): ChatRoom?
+}
+
+interface ChatRoomCustomRepository {
+    fun findChatRoomWithCategoryIds(
+        categoryIds: List<Long>,
+        pageable: Pageable,
+    ): Slice<ChatRoom>
+}
+
+class ChatRoomCustomRepositoryImpl(
+    private val kotlinJdslJpqlExecutor: KotlinJdslJpqlExecutor,
+) : ChatRoomCustomRepository {
+    override fun findChatRoomWithCategoryIds(
+        categoryIds: List<Long>,
+        pageable: Pageable,
+    ): Slice<ChatRoom> =
+        kotlinJdslJpqlExecutor.getSlice(pageable) {
+            select(entity(ChatRoom::class))
+                .from(entity(ChatRoom::class), innerJoin(ChatRoom::categories))
+                .where(path(ChatRoomCategory::category).path(Category::id).`in`(categoryIds))
+                .groupBy(path(ChatRoom::id))
+                .having(count(path(ChatRoomCategory::id)).eq(categoryIds.size.toLong()))
+        }
 }
