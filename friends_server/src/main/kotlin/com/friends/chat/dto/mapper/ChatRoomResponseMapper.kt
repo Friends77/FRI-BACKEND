f47f3c01403c97895fe02c799db226e458d0a8a7
Chae <oumuamua121@gmail.com>
2025-01-22 00:49:17 +0900

    feat: 채팅방 인원 조회 API

--- friends_server/src/main/kotlin/com/friends/chat/dto/mapper/ChatRoomResponseMapper.kt
+++ friends_server/src/main/kotlin/com/friends/chat/dto/mapper/ChatRoomResponseMapper.kt
@@ -2,43 +2,68 @@ package com.friends.chat.dto.mapper
 
 import com.friends.chat.dto.ChatRoomDetailResponseDto
 import com.friends.chat.dto.ChatRoomInfoResponseDto
+import com.friends.chat.dto.ChatRoomMemberInfoResponseDto
 import com.friends.chat.entity.ChatRoom
 import com.friends.chat.entity.ChatRoomMember
 import com.friends.common.mapper.toCategoryInfoResponse
+import com.friends.friendship.entity.FriendshipRequestStatusEnums
+import com.friends.member.entity.Member
 import com.friends.message.entity.Message
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 
-fun toChatRoomInfoResponse(
-    chatRoomMember: ChatRoomMember,
-    memberCount: Int,
-    representativeProfile: List<String>,
-    unreadMessageCount: Int,
-    lastMessage: Message?,
-    imageUrl: String,
-): ChatRoomInfoResponseDto =
-    ChatRoomInfoResponseDto(
-        chatRoomMemberId = chatRoomMember.id,
-        id = chatRoomMember.chatRoom.id,
-        title = chatRoomMember.chatRoom.title,
+@Component
+class ChatRoomResponseMapper(
+    @Value("\${image.profile-base-url}")
+    private val profileBaseImageUrl: String,
+) {
+    fun toChatRoomInfoResponse(
+        chatRoomMember: ChatRoomMember,
+        memberCount: Int,
+        representativeProfile: List<String>,
+        unreadMessageCount: Int,
+        lastMessage: Message?,
+        imageUrl: String,
+    ): ChatRoomInfoResponseDto =
+        ChatRoomInfoResponseDto(
+            chatRoomMemberId = chatRoomMember.id,
+            id = chatRoomMember.chatRoom.id,
+            title = chatRoomMember.chatRoom.title,
+            imageUrl = imageUrl,
+            categoryIdList = chatRoomMember.chatRoom.categories.map { toCategoryInfoResponse(it.category) },
+            participantCount = memberCount,
+            participantProfileList = representativeProfile,
+            lastMessageTime = lastMessage?.createdAt,
+            lastMessage = lastMessage?.content,
+            unreadMessageCount = unreadMessageCount,
+        )
+
+    fun toChatRoomDetailResponseDto(
+        chatRoom: ChatRoom,
+        memberCount: Int,
+        isLike: Boolean,
+        imageUrl: String,
+    ) = ChatRoomDetailResponseDto(
+        id = chatRoom.id,
+        title = chatRoom.title,
         imageUrl = imageUrl,
-        categoryIdList = chatRoomMember.chatRoom.categories.map { toCategoryInfoResponse(it.category) },
+        categoryIdList = chatRoom.categories.map { toCategoryInfoResponse(it.category) },
         participantCount = memberCount,
-        participantProfileList = representativeProfile,
-        lastMessageTime = lastMessage?.createdAt,
-        lastMessage = lastMessage?.content,
-        unreadMessageCount = unreadMessageCount,
+        likeCount = chatRoom.likeCount,
+        isLike = isLike,
     )
 
-fun toChatRoomDetailResponseDto(
-    chatRoom: ChatRoom,
-    memberCount: Int,
-    isLike: Boolean,
-    imageUrl: String,
-) = ChatRoomDetailResponseDto(
-    id = chatRoom.id,
-    title = chatRoom.title,
-    imageUrl = imageUrl,
-    categoryIdList = chatRoom.categories.map { toCategoryInfoResponse(it.category) },
-    participantCount = memberCount,
-    likeCount = chatRoom.likeCount,
-    isLike = isLike,
-)
+    fun toChatRoomMemberInfoResponseDto(
+        member: Member,
+        friendshipRequestStatusEnums: FriendshipRequestStatusEnums,
+        isManager: Boolean,
+        isMe: Boolean,
+    ) = ChatRoomMemberInfoResponseDto(
+        id = member.id,
+        nickname = member.nickname,
+        profileImageUrl = member.profile?.imageUrl ?: profileBaseImageUrl,
+        friendshipStatusEnums = friendshipRequestStatusEnums,
+        isManager = isManager,
+        isMe = isMe,
+    )
+}
