2a9c3badc0eba643f699fa2dedbc2eca1d79f7c6
Hosung <an3735297@naver.com>
2025-01-23 13:56:59 +0900

    remove : enumerated 애노테이션 삭제

--- friends_server/src/main/kotlin/com/friends/message/entity/Message.kt
+++ friends_server/src/main/kotlin/com/friends/message/entity/Message.kt
@@ -5,8 +5,6 @@ import com.friends.common.entity.BaseTimeEntity
 import com.friends.member.entity.Member
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -29,7 +27,6 @@ class Message(
     @Column(nullable = false)
     var content: String,
     @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
     var type: MessageType,
 ) : BaseTimeEntity() {
     companion object {
