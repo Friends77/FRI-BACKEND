2a5ba0e4499b374238791be6945b5e6345d0356b
Chae <oumuamua121@gmail.com>
2025-01-24 16:24:12 +0900

    feat: 채팅방 특정 멤버 조회 API

--- friends_server/src/main/kotlin/com/friends/chat/service/ChatRoomQueryService.kt
+++ friends_server/src/main/kotlin/com/friends/chat/service/ChatRoomQueryService.kt
@@ -1,5 +1,6 @@
 package com.friends.chat.service
 
+import com.friends.chat.ChatRoomMemberNotFoundException
 import com.friends.chat.ChatRoomNotFoundException
 import com.friends.chat.dto.ChatRoomDetailResponseDto
 import com.friends.chat.dto.ChatRoomInfoResponseDto
@@ -73,7 +74,7 @@ class ChatRoomQueryService(
     }
 
     @Transactional(readOnly = true)
-    fun getChatRoomMemberInfo(
+    fun getChatRoomMemberInfoList(
         chatRoomId: Long,
         memberId: Long,
     ): List<ChatRoomMemberInfoResponseDto> {
@@ -102,6 +103,20 @@ class ChatRoomQueryService(
                 }
         chatRoomMemberList.addAll(members)
         return chatRoomMemberList
+    }
+
+    fun getChatRoomMemberInfo(
+        chatRoomId: Long,
+        requesterId: Long,
+        newMemberId: Long,
+    ): ChatRoomMemberInfoResponseDto {
+        val chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow { throw ChatRoomNotFoundException() }
+        val requester = memberRepository.findById(requesterId).orElseThrow { throw MemberNotFoundException() }
+        val newMember = memberRepository.findById(newMemberId).orElseThrow { throw MemberNotFoundException() }
+        if (!chatRoomMemberRepository.existsChatRoomMemberByChatRoomAndMember(chatRoom, newMember) || !chatRoomMemberRepository.existsChatRoomMemberByChatRoomAndMember(chatRoom, requester)) {
+            throw ChatRoomMemberNotFoundException()
+        }
+        return chatRoomResponseMapper.toChatRoomMemberInfoResponseDto(newMember, memberFriendshipStatus(member = requester, friend = newMember), isManager = false, isMe = false)
     }
 
     private fun memberFriendshipStatus(
